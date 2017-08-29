e10ef792eb50f4424899463d86fe76bd57fabc43
gerry <girtri@libero.it>
2017-08-29 17:59:07 +0200

    Cap 5, pag 174. (the second tab panel will be quite similar..)

---
+++ js/Components/Main.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import {Tabs, Tab} from "material-ui/Tabs";
+import FontIcon from "material-ui/FontIcon";
+import TitleBar from "./TitleBar.jsx";
+import ScreenshotTab from "./ScreenshotTab.jsx";
+import AnimationTab from "./AnimationTab.jsx";
+
+class Main extends Component 
+{
+	render() {
+		const ScreenshotIcon = <FontIcon className="materialicons">camera_alt</FontIcon>;
+		const AnimationIcon = <FontIcon className="materialicons">video_call</FontIcon>;
+		
+		return (
+			<div>
+				<TitleBar />
+				<Tabs>
+					<Tab icon={ScreenshotIcon} label="SCREENSHOT" />
+					<Tab icon={AnimationIcon} label="ANIMATION" />
+				</Tabs>
+				<div>
+				{true
+					? <ScreenshotTab />
+					: <AnimationTab />
+				}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Main;
---
+++ js/Containers/App.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import Main from "../Components/Main.jsx";
+import {deepOrange500} from "material-ui/styles/colors";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+	palette: {
+		accent1Color: deepOrange500
+	}
+});
+
+export default class App extends Component 
+{
+	render() {
+		return (
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<Main />
+			</MuiThemeProvider>
+		);
+	}
+}
